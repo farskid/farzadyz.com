514a17048f06cba01157844d73164cd599a554b8
Farzad YZ <farskid@gmail.com>
2018-08-10 00:11:29 +0300

    v2

--- README.md
+++
@@ -1,3 +0,0 @@
-![Travis Build](https://travis-ci.org/farskid/farzadyz.com.svg?branch=master)
-
-My Personal Blog written with Jekyll, hosted on Github pages and Managed via Travis ;)
--- assets/source-maps/_assets/js/site.js
+++
@@ -1,9 +0,0 @@
-function captureEvent(cb) {
-  cb && cb();
-  return false;
-}
-function anchorScroll() {
-  var height = document.querySelector('header').offsetHeight;
-
-  window.scrollTo(0, height, 500);
-}
