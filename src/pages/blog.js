30df7c675541793d8c3fc9cb61d1ac9b8bef2e6f
Farzad YZ <farskid@gmail.com>
2018-12-24 22:36:33 +0200

    fix lowercase imports

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,8 +1,8 @@
 import React from "react";
 import { Link, graphql } from "gatsby";
 
-import Layout from "../components/layout";
-import SEO from "../components/seo";
+import Layout from "../components/Layout";
+import SEO from "../components/Seo";
 import { rhythm } from "../utils/typography";
 
 class BlogIndex extends React.Component {
