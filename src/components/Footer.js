0be912e4e01636f4cb16b64ca0c9995036730d47
Farzad YZ <farskid@gmail.com>
2019-05-21 20:18:36 +0300

    fix responsive styles for iPhone SE

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,21 +1,10 @@
 import React from "react";
 import { rhythm } from "../utils/typography";
+import "./Footer.css";
 
 export function Footer() {
   return (
-    <footer
-      style={{
-        fontSize: "0.8em",
-        textAlign: "center",
-        marginTop: rhythm(4),
-        marginRight: rhythm(-3 / 4),
-        marginBottom: rhythm(-1.5),
-        marginLeft: rhythm(-3 / 4),
-        padding: `${rhythm(0.75)} 0`,
-        color: "#777",
-        fontWeight: 600
-      }}
-    >
+    <footer className="footer">
       The materials of this website are licensed under
       <a
         target="_blank"
--- src/components/Layout.js
+++ src/components/Layout.js
@@ -9,14 +9,7 @@ class Layout extends React.Component {
     const { children, title, description, CustomSEO } = this.props;
 
     return (
-      <div
-        style={{
-          marginLeft: `auto`,
-          marginRight: `auto`,
-          maxWidth: rhythm(24),
-          padding: `${rhythm(1.5)} ${rhythm(3 / 4)}`
-        }}
-      >
+      <div className="layout-container">
         {!!CustomSEO ? (
           <CustomSEO />
         ) : (
