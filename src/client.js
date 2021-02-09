a9bfc1e02f2a5a0d955fc26c9a7894dc0ffe6411
Farzad YZ <farskid@gmail.com>
2021-02-09 13:30:07 +0200

    only prod analytics

--- src/client.js
+++ src/client.js
@@ -2,7 +2,6 @@ import * as sapper from "@sapper/app";
 import splitbee from "@splitbee/web";
 
 splitbee.init({
-  token: "6A49KS32G9TE",
   disableCookie: true,
   scriptUrl: "/bee.js",
   apiUrl: "/_hive",
