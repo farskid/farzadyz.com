e98d826ce5c79bfde7c8f5da7978e98b6a61dbfd
Farzad YZ <farskid@gmail.com>
2021-02-09 11:27:31 +0200

    move splitbee to client.js

--- src/client.js
+++ src/client.js
@@ -1,4 +1,7 @@
 import * as sapper from "@sapper/app";
+import splitbee from "@splitbee/web";
+
+splitbee.init();
 
 sapper.start({
   target: document.querySelector("[data-root]"),
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -2,13 +2,6 @@
   import metadata from "../../content/data/metadata.json";
   import Layout from "../components/Layout.svelte";
   import Bio from "../components/Bio.svelte";
-
-  import splitbee from "@splitbee/web";
-  import { onMount } from "svelte";
-
-  onMount(() => {
-    splitbee.init();
-  });
 </script>
 
 <Layout title={metadata.siteTitle} description={metadata.siteDescription}>
