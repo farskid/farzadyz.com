6034080f15b5b408a2b1aa666550b724a41668f3
Farzad Yousefzadeh <farskid@gmail.com>
2021-12-10 22:42:39 +0200

    add public icons

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -54,10 +54,27 @@ function MyApp({ Component, pageProps }: AppProps) {
             title="Subscribe to farzadyz.com/blog"
             href="/feeds/feed.json"
           />
-          <link rel="icon" href="/favicon.ico" sizes="any" />
-          <link rel="icon" href="/icon.svg" type="image/svg+xml" />
-          <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
-          <link rel="manifest" href="/manifest.webmanifest" />
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="/apple-touch-icon.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="/favicon-32x32.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href="/favicon-16x16.png"
+          />
+          <link rel="manifest" href="/site.webmanifest" />
+          <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+          <link rel="shortcut icon" href="/favicon.ico" />
+          <meta name="theme-color" content="#ffffff" />
           {/* <script
             defer
             data-domain="stately.ai"
