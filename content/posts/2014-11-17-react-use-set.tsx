8e96b06becf35e268d86656649085d8b8c36f45e
Farzad Yousefzadeh <farskid@gmail.com>
2024-11-17 23:29:42 +0200

    Update 2014-11-17-react-use-set.tsx

--- content/posts/2014-11-17-react-use-set.tsx
+++ content/posts/2014-11-17-react-use-set.tsx
@@ -1,3 +1,16 @@
+---
+title: Uset `set` as a state primitive in useState
+description: >-
+  There comes times when you'd like to use a `Set` as the primitive data type in React. Specially with even simpler hooks such as `useState` that can be challenging
+publishedAt: "2024-11-17"
+tags:
+  - javascript
+  - typescript
+  - react
+  - set
+id: a65819c4-3dbf-444f-81ed-418c5c3a495j
+---
+
 There comes times when you'd like to use a `Set` as the primitive data type in React. Specially with even simpler hooks such as `useState` that can be challenging.
 The main challenge is because React defaults to immutable updates and `Set` (as well as `Map`) are mutative data types by design. Therefore there needs to be a small abstraction
 that tells React to treat the data type as if it's immutable while keeping the underlying data type as a mutative `Set`.
